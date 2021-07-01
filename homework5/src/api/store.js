31032ec9e372770f7d739bf5052274858b928a15
Kate Vovk <63584116+kate-vovk@users.noreply.github.com>
2021-07-02 00:08:38 +0300

    Delete homework5 directory

--- homework5/src/api/store.js
+++
@@ -1,7 +0,0 @@
-import Fetcher from "../service/Fetcher";
-
-export const getElectronics = (options={}) => {
-    const {limit=5} = options;
-    console.log(limit);
-    return Fetcher.get(`/products/category/electronics?limit=${limit}`, limit);
-}
--- homework5/src/service/Fetcher.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = 'https://fakestoreapi.com';
-
-const fetcher = ({url, options}) => {
-   return fetch(`${BASE_URL}${url}`, options).then(response => response.json()).catch(err => {console.error(err.message);})
-}
-
-export default {
-    get:(url, options) => fetcher({method: 'GET', url, ...options}),
-}
